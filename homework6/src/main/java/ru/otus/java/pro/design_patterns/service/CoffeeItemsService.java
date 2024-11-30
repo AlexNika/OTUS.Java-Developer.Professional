b437c2e71f440b66ad90a7f59b22acbed69e4e6d
Alexander Nikolaev <alexander.nikolaev@gmail.com>
2024-12-01 00:58:17 +0300

    Исправления по замечаниям преподавателя. Transactions moved to CoffeeItemsServiceProxy.

--- homework6/src/main/java/ru/otus/java/pro/design_patterns/service/CoffeeItemsService.java
+++ homework6/src/main/java/ru/otus/java/pro/design_patterns/service/CoffeeItemsService.java
@@ -13,12 +13,10 @@ import java.util.List;
 public class CoffeeItemsService implements ItemsService<Coffee> {
     private static final Logger logger = LogManager.getLogger(CoffeeItemsService.class);
 
-    private final Connection connection;
     private final CoffeeItemsDao coffeeItemsDao;
 
     public CoffeeItemsService(Connection connection) {
-        this.connection = connection;
-        this.coffeeItemsDao = new CoffeeItemsDao(this.connection);
+        this.coffeeItemsDao = new CoffeeItemsDao(connection);
     }
 
     @Override
@@ -28,38 +26,20 @@ public class CoffeeItemsService implements ItemsService<Coffee> {
         coffeeItemsDao.createTable();
         coffeeItemsDao.generateData(qty);
         coffeeItems = coffeeItemsDao.getGeneratedData();
-        try {
-            connection.setAutoCommit(false);
-            for (Coffee coffeeItem : coffeeItems) {
-                insertedItemsQty += coffeeItemsDao.insertData(coffeeItem);
-            }
-            connection.commit();
-            logger.info("Rows inserted into the database table: {}", insertedItemsQty);
-        } catch (SQLException e) {
-            connection.rollback();
-            throw e;
-        } finally {
-            connection.setAutoCommit(true);
+        for (Coffee coffeeItem : coffeeItems) {
+            insertedItemsQty += coffeeItemsDao.insertData(coffeeItem);
         }
+        logger.info("Rows inserted into the database table: {}", insertedItemsQty);
     }
 
     @Override
     public void updateDataPrice() throws SQLException {
         int updatedItemsQty = 0;
-        try {
-            connection.setAutoCommit(false);
-            List<Coffee> coffeeItemsFromDB = coffeeItemsDao.readData();
-            for (Coffee coffeeItem : coffeeItemsFromDB) {
-                BigDecimal price = coffeeItem.price().multiply(BigDecimal.valueOf(2));
-                updatedItemsQty += coffeeItemsDao.updatePriceData(coffeeItem, price);
-            }
-            connection.commit();
-            logger.info("Rows updated in the database table: {}", updatedItemsQty);
-        } catch (SQLException e) {
-            connection.rollback();
-            throw e;
-        } finally {
-            connection.setAutoCommit(true);
+        List<Coffee> coffeeItemsFromDB = coffeeItemsDao.readData();
+        for (Coffee coffeeItem : coffeeItemsFromDB) {
+            BigDecimal price = coffeeItem.price().multiply(BigDecimal.valueOf(2));
+            updatedItemsQty += coffeeItemsDao.updatePriceData(coffeeItem, price);
         }
+        logger.info("Rows updated in the database table: {}", updatedItemsQty);
     }
 }
--- homework6/src/main/java/ru/otus/java/pro/design_patterns/service/CoffeeItemsServiceProxy.java
+++ homework6/src/main/java/ru/otus/java/pro/design_patterns/service/CoffeeItemsServiceProxy.java
@@ -10,9 +10,11 @@ import java.sql.SQLException;
 public class CoffeeItemsServiceProxy implements ItemsService<Coffee> {
     private static final Logger logger = LogManager.getLogger(CoffeeItemsServiceProxy.class);
 
+    private final Connection connection;
     private final CoffeeItemsService coffeeItemsService;
 
     public CoffeeItemsServiceProxy(Connection connection) {
+        this.connection = connection;
         coffeeItemsService = new CoffeeItemsService(connection);
     }
 
@@ -20,13 +22,29 @@ public class CoffeeItemsServiceProxy implements ItemsService<Coffee> {
     public void populateData(int qty) throws SQLException {
         logger.info("Proxy pattern applied. " +
                 "Method populateData({}) was launched from the class CoffeeItemsServiceProxy", qty);
-        coffeeItemsService.populateData(qty);
+        try {
+            connection.setAutoCommit(false);
+            coffeeItemsService.populateData(qty);
+        } catch (SQLException e) {
+            connection.rollback();
+            logger.error("Error. Transaction 'populateData' cannot be completed. Transaction rollback", e);
+        } finally {
+            connection.setAutoCommit(true);
+        }
     }
 
     @Override
     public void updateDataPrice() throws SQLException {
         logger.info("Proxy pattern applied. " +
                 "Method updateDataPrice() was launched from the class CoffeeItemsServiceProxy");
-        coffeeItemsService.updateDataPrice();
+        try {
+            connection.setAutoCommit(false);
+            coffeeItemsService.updateDataPrice();
+        } catch (SQLException e) {
+            connection.rollback();
+            logger.error("Error. Transaction 'updateDataPrice' cannot be completed. Transaction rollback", e);
+        } finally {
+            connection.setAutoCommit(true);
+        }
     }
 }
